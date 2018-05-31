23a184e72ba29282e1dc7bf63e954a12e3bc28bb
Alexey.Zavgorodniiy <Lelik1986>
2018-05-31 15:46:12 +0300

    issue-01: all functionality is done

--- core/src/main/java/model/Availability.java
+++ core/src/main/java/model/Availability.java
@@ -3,16 +3,10 @@ package model;
 import java.util.Arrays;
 
 public enum Availability {
-    ABSENT("ABSENT"),
-    PRESENT("PRESENT");
-
-    private String name;
+    ABSENT,
+    PRESENT;
 
     Availability() {
-    }
-
-    private Availability(final String name) {
-        this.name = name;
     }
 
     public static Availability of(String availability) {
@@ -21,14 +15,5 @@ public enum Availability {
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(
                         "Cannot find availability by name: " + availability));
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public String toString() {
-        return getName();
     }
 }
--- web/src/main/java/controller/AvailabilityController.java
+++ web/src/main/java/controller/AvailabilityController.java
@@ -3,6 +3,7 @@ package controller;
 import java.util.Arrays;
 import java.util.List;
 import model.Availability;
+import model.Category;
 import model.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import repository.ItemRepository;
 
@@ -24,23 +26,25 @@ public class AvailabilityController {
     }
 
     @GetMapping("/availabilitySelection")
-    public ModelAndView showAllAvailableItems() {
+    public ModelAndView showAvailabilitySelectionPage() {
         ModelAndView modelAndView = new ModelAndView("availabilitySelection");
         modelAndView.addObject("availabilities", Arrays.asList(Availability.values()));
-        modelAndView.addObject("selectedAvail", "");
+        modelAndView.addObject("selectedItemAvailability", new Item());
         return modelAndView;
     }
 
     @PostMapping("/availabilitySelection")
-    public String selectedAvailability(@ModelAttribute String selectedAvail) {
-        return "redirect:" + "/availabilities/" + selectedAvail;
+    public String selectCategory(@ModelAttribute Item item) {
+        Availability availability = item.getAvailability();
+        return "redirect:/availabilities/" + availability.toString();
     }
 
-    @GetMapping("/availabilities/{selectedAvail}")
-    public ModelAndView showAvailabilities(@PathVariable String selectedAvail) {
+
+    @GetMapping("/availabilities/{str}")
+    public ModelAndView selectedCategory(@PathVariable String str) {
         ModelAndView modelAndView = new ModelAndView("availabilities");
-        Availability availabilityOf = Availability.of(selectedAvail);
-        List<Item> items = itemRepository.findAllItemsByAvailability(availabilityOf);
+        Availability availability = Availability.of(str);
+        List<Item> items = itemRepository.findAllItemsByAvailability(availability);
         modelAndView.addObject("items", items);
         return modelAndView;
     }
